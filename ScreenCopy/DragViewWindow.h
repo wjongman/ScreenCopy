a1c49dc384c70a8576ec500f4f1b2447a340a862
wjongman <56084366+wjongman@users.noreply.github.com>
2020-09-18 02:10:50 +0200

    experimenting with image preview

--- ScreenCopy/DragViewWindow.h
+++ ScreenCopy/DragViewWindow.h
@@ -3,16 +3,16 @@
 /////////////////////////////////////////////////////////////////////////////
 
 #pragma once
-#include <atlscrl.h>
-// #include "DragDropSource.h"
-#include "DropSource.h"
+#include "DragSource.h"
+
+#include <gdiplus.h>
+#pragma comment(lib, "Gdiplus.lib")
 
 class CDragViewWindow : public CWindowImpl<CDragViewWindow, CWindow,
-    CWinTraits<WS_BORDER | WS_SYSMENU | WS_THICKFRAME/*, WS_EX_TOOLWINDOW*/> >
+    CWinTraits<WS_BORDER | WS_SYSMENU | WS_THICKFRAME, WS_EX_TOOLWINDOW> >
 {
-    HDC m_memDC = NULL; // Memory DC for buffered drawing of our bitmap
+    ULONG_PTR m_gdiplusToken;
     bool m_bDragging = false;
-    CPoint m_ptLastMouse = { 0 };
     std::wstring m_dragFilePath;
 
 public:
@@ -24,81 +24,48 @@ public:
         return FALSE;
     }
 
-    void DoPaint(CDCHandle dc)
-    {
-        //TODO: Add your drawing code here
-    }
-
     BEGIN_MSG_MAP(CDragViewWindow)
-    MESSAGE_HANDLER(WM_CREATE, OnCreate)
-    MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
-//     MESSAGE_HANDLER(WM_CLOSE, OnClose)
-    MESSAGE_HANDLER(WM_PAINT, OnPaint)
-    MESSAGE_HANDLER(WM_LBUTTONDOWN, OnLButtonDown)
-    MESSAGE_HANDLER(WM_MOUSEMOVE, OnMouseMove)
+        MESSAGE_HANDLER(WM_CREATE, OnCreate)
+        MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
+        MESSAGE_HANDLER(WM_PAINT, OnPaint)
+        MESSAGE_HANDLER(WM_LBUTTONDOWN, OnLButtonDown)
+        MESSAGE_HANDLER(WM_MOUSEMOVE, OnMouseMove)
     END_MSG_MAP()
 
-// Handler prototypes (uncomment arguments if needed):
-//  LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
-//  LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
-//  LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
-
-//-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     void SetDragFilePath(std::wstring const& filePath)
     {
         m_dragFilePath = filePath;
     }
 
-//-------------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     LRESULT OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM lParam, BOOL& /*bHandled*/)
     {
-        // At this stage we have a valid HWND so now we can get
-        // our device context and use it to create a memory DC
-        HDC dc = ::GetDC(m_hWnd);
-        m_memDC = ::CreateCompatibleDC(dc);
-        ::ReleaseDC(m_hWnd, dc);
-
         CRect rcWindow = { 0, 0, 64, 64 };
         SetWindowPos(HWND_TOP, &rcWindow, SW_HIDE);
         CenterWindow();
-//         ShowWindow(SW_SHOW);
+        SetWindowText(L"drag");
+
+        // Initialize GDI+.
+        Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+        Gdiplus::GdiplusStartup(&m_gdiplusToken, &gdiplusStartupInput, NULL);
+
         return 0;
     }
 
     //-------------------------------------------------------------------------
     LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled)
     {
-        ::DeleteDC(m_memDC);
-        // Avoid losing foreground focus
+        Gdiplus::GdiplusShutdown(m_gdiplusToken);
+        ::DeleteFile(m_dragFilePath.c_str());
         bHandled = FALSE;
         return 1;
-    }
-
-    //-------------------------------------------------------------------------
-    LRESULT OnClose(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled)
-    {
-        bHandled = FALSE;
-        return 1;
-    }
-
-     //-------------------------------------------------------------------------
-    LRESULT OnPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
-    {
-        CPaintDC dc(m_hWnd);
-
-        // Draw buffered bitmap
-//         CRect rcROI = GetROI();
-//         ::BitBlt(dc, 0, 0, rcROI.Width(), rcROI.Height(), m_memDC, 0, 0, SRCCOPY);
-
-        return 0;
     }
 
      //-------------------------------------------------------------------------
     LRESULT OnLButtonDown(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM lParam, BOOL& /*bHandled*/)
     {
         m_bDragging = true;
-        m_ptLastMouse = CPoint(LOWORD(lParam), HIWORD(lParam));
-//         SetCapture();
         return 0;
     }
     
@@ -107,16 +74,6 @@ public:
     {
         if (m_bDragging)
         {
-            CPoint ptMouse(LOWORD(lParam), HIWORD(lParam));
-            int deltaX = ptMouse.x - m_ptLastMouse.x;
-            int deltaY = ptMouse.y - m_ptLastMouse.y;
-
-//             // Adjust ROI
-//             CRect rcROI = GetROI();
-//             rcROI.OffsetRect(deltaX, deltaY);
-//             SetROI(rcROI);
-
-            m_ptLastMouse = ptMouse;
             SetCapture();
             BeginDrag(m_dragFilePath);
             ReleaseCapture();
@@ -126,63 +83,68 @@ public:
         return 0;
     }
 
+     //-------------------------------------------------------------------------
     LRESULT BeginDrag(std::wstring const& filePath)
     {
-        IDataObject* pdto;
-        if (SUCCEEDED(GetUIObjectOfFile(m_hWnd, filePath.c_str(), IID_IDataObject, (void**)&pdto)))
-        {
-            IDropSource* pds = new CDropSource();
-            if (pds)
+        CComPtr<IDataObject> spDataObject;
+        if (SUCCEEDED(GetUIObjectOfFile(m_hWnd, filePath.c_str(), IID_PPV_ARGS(&spDataObject))))
+        { 
+            IDropSource* pDropSource = new CDropSource(); // Will delete itself when refcount becomes 0
+            if (pDropSource)
             {
                 DWORD dwEffect;
-                DoDragDrop(pdto, pds, DROPEFFECT_COPY | DROPEFFECT_LINK, &dwEffect);
-                pds->Release();
+                DoDragDrop(spDataObject, pDropSource, DROPEFFECT_COPY | DROPEFFECT_LINK, &dwEffect);
+                pDropSource->Release();
             }
-            pdto->Release();
         }
         return 0;
     }
 
-//     LRESULT BeginDrag2()
-//     {
-//         //         NMLISTVIEW* pnmlv = (NMLISTVIEW*)phdr;
-//         //        std::vector<CDraggedFileInfo> vec;
-// 
-//         std::wstring testFilePath{ L"C:/etc/_Test/thumbnails/test2.png" };
-//         std::vector<std::wstring> vec = { L"C:/etc/_Test/thumbnails/test2.png" };
-// 
-//         CComObjectStack<CDragDropSource> dropsrc;
-//         DWORD dwEffect = 0;
-//         HRESULT hr;
-//         CComPtr<IDragSourceHelper> pdsh;
-// 
-//         // Init the drag/drop data object.
-//         if (!dropsrc.Init(testFilePath))
-//         {
-//             ATLTRACE("Error: Couldn't init drop source object\n");
-//             return 0; // do nothing
-//         }
-// 
-//         // On 2K+, init a drag source helper object that will do the fancy drag
-//         // image when the user drags into Explorer (or another target that supports
-//         // the drag/drop helper).
-//         hr = pdsh.CoCreateInstance(CLSID_DragDropHelper);
-// 
-//         if (SUCCEEDED(hr))
-//         {
-//             CComQIPtr<IDataObject> pdo;
-//             CPoint ptStart{ 32, 32 };
-//             if (pdo = dropsrc.GetUnknown())
-//                 pdsh->InitializeFromWindow(m_hWnd, &ptStart, pdo);
-//         }
-// 
-//         // Start the drag/drop!
-//         hr = dropsrc.DoDragDrop(DROPEFFECT_COPY, &dwEffect);
-// 
-//         if (FAILED(hr))
-//             ATLTRACE("Error: DoDragDrop() failed, error: 0x%08X\n", hr);
-// 
-//         return 0;
-//     }
+     //-------------------------------------------------------------------------
+    LRESULT OnPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM lParam, BOOL& /*bHandled*/)
+    {
+        CPaintDC dc(m_hWnd);
+
+        CRect rcClient;
+        GetClientRect(&rcClient);
+        Gdiplus::Bitmap bitmap(m_dragFilePath.c_str());
+
+        int cx = bitmap.GetWidth();
+        int cy = bitmap.GetHeight();
+
+
+        CDC memDC = CreateCompatibleDC(dc);
+        HBITMAP hBmp;
+        bitmap.GetHBITMAP(RGB(0, 0, 0), &hBmp);
+        CBitmapHandle bmpOld = memDC.SelectBitmap(hBmp);
+
+        CRect rcPage{ 0, 0, 64, 64 };
+        // calc scaling factor, so that image is not too small
+        // (based on the width only, max 3/4 width)
+        int nScale = ::MulDiv(rcPage.right, 3, 4) / cx;
+        if (nScale == 0) // too big already
+            nScale = 1;
+        // calc margins to center bitmap
+        int xOff = (rcPage.right - nScale * cx) / 2;
+        if (xOff < 0)
+            xOff = 0;
+        int yOff = (rcPage.bottom - nScale * cy) / 2;
+        if (yOff < 0)
+            yOff = 0;
+        // ensure that preview doesn't go outside of the page
+        int cxBlt = nScale * cx;
+        if (xOff + cxBlt > rcPage.right)
+            cxBlt = rcPage.right - xOff;
+        int cyBlt = nScale * cy;
+        if (yOff + cyBlt > rcPage.bottom)
+            cyBlt = rcPage.bottom - yOff;
+
+        // paint bitmap image
+        ::StretchBlt(dc, xOff, yOff, cxBlt, cyBlt, memDC, 0, 0, cx, cy, SRCCOPY);
+
+        memDC.SelectBitmap(bmpOld);
+        return 0;
+    }
+    
 
 };
