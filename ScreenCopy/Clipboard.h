7e2635eede602a08f15eb09f6c9e46fc5c297175
wjongman <56084366+wjongman@users.noreply.github.com>
2020-09-19 22:46:28 +0200

    renaming

--- ScreenCopy/Clipboard.h
+++ ScreenCopy/Clipboard.h
@@ -5,56 +5,50 @@ class Clipboard
 {
 public:
     //-------------------------------------------------------------------------
-    static void Write(CString sPuzzle)
+    static void Write(std::wstring text)
     {
         if (::OpenClipboard(NULL))
         {
-            EmptyClipboard();
-
-            int cch = sPuzzle.GetLength();
             // Allocate a global memory object for the text. 
-
-            HGLOBAL hglbCopy = GlobalAlloc(GMEM_MOVEABLE, (cch + 1) * sizeof(TCHAR));
-            if (hglbCopy)
+            int bufSize = text.size();
+            HGLOBAL hGlobal = ::GlobalAlloc(GMEM_MOVEABLE, (bufSize + 1) * sizeof(wchar_t));
+            if (hGlobal)
             {
                 // Lock the handle and copy the text to the buffer. 
-                LPTSTR lptstrCopy = reinterpret_cast<TCHAR*>(GlobalLock(hglbCopy));
-                if (lptstrCopy)
+                wchar_t* pTextCopy = reinterpret_cast<wchar_t*>(::GlobalLock(hGlobal));
+                if (pTextCopy)
                 {
-                    memcpy(lptstrCopy, sPuzzle, cch * sizeof(TCHAR));
-                    lptstrCopy[cch] = (TCHAR)0;    // null character 
+                    memcpy(pTextCopy, text.c_str(), bufSize * sizeof(wchar_t));
+                    pTextCopy[bufSize] = L'\0';
                 }
-                GlobalUnlock(hglbCopy);
+                ::GlobalUnlock(hGlobal);
 
-                // Place the handle on the clipboard. 
-                SetClipboardData(CF_UNICODETEXT, hglbCopy);
-
-                CloseClipboard();
+                ::EmptyClipboard();
+                ::SetClipboardData(CF_UNICODETEXT, hGlobal);
+                ::CloseClipboard();
             }
-
         }
     }
 
     //-------------------------------------------------------------------------
-    static CString Read()
+    static std::wstring Read()
     {
-        CString sPuzzle;
-
+        std::wstring result;
         if (::IsClipboardFormatAvailable(CF_TEXT) && ::OpenClipboard(NULL))
         {
             HANDLE hClipData = ::GetClipboardData(CF_UNICODETEXT);
             if (hClipData)
             {
-                TCHAR* pClipText = reinterpret_cast<TCHAR*>(GlobalLock(hClipData));
+                wchar_t* pClipText = reinterpret_cast<wchar_t*>(::GlobalLock(hClipData));
                 if (pClipText != NULL)
                 {
-                    sPuzzle = pClipText;
-                    GlobalUnlock(hClipData);
+                    result = pClipText;
+                    ::GlobalUnlock(hClipData);
                 }
             }
             ::CloseClipboard();
         }
-        return sPuzzle;
+        return result;
     }
 };
 
